317cf25d0e6f270bb5f8a4390e06bc00715d847b
rkartha1987 <rkartha1987@gmail.com>
2021-10-13 12:26:16 +0530

    Change-Id: I0000000000000000000000000000000000000000 manage faculty detail page commit
    Change-Id: Idb7996c687230c7c8792c9ae83d09bd64caf6aca

--- BITSAUTOMATION/src/test/java/pageObjects/ManageFaculty_Detail_Page_Objects.java
+++ BITSAUTOMATION/src/test/java/pageObjects/ManageFaculty_Detail_Page_Objects.java
@@ -31,47 +31,47 @@ public class ManageFaculty_Detail_Page_Objects {
 	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[3]/div/select")
 	public static WebElement AddGender;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[4]/div/select")
+		@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[4]/div/select")
 	public static WebElement AddDepart;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[5]/div/select")
+		@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[5]/div/input")
+		public static WebElement AreaSpclzn;
+		
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[6]/div/select")
 	public static WebElement AddCampus;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[6]/div/input")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[7]/div/input")
 	public static WebElement AddPEmail;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[7]/div/input")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[8]/div/input")
 	public static WebElement AddOEmail;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[8]/div/input")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[9]/div/input")
 	public static WebElement AddMNumber;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[9]/div/input")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[10]/div/input")
 	public static WebElement AddLNumber;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[10]/div/select")
-	public static WebElement AddFType;
-	
 	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[11]/div/input")
+	public static WebElement AddHomeTown ;
+
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[12]/div/input")
 	public static WebElement AddAddress1;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[12]/div/input")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[13]/div/input")
 	public static WebElement AddAddress2;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[13]/div/select")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[14]/div/select")
 	public static WebElement AddCountry;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[14]/div/select")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[15]/div/select")
 	public static WebElement AddState;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[15]/div/div/div/select")
+	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[16]/div/div/div/select")
 	public static WebElement AddCity;
 	
-	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[16]/div/input")
-	public static WebElement AddPIN;
-	
 	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[17]/div/input")
-	public static WebElement AddHometown;
+	public static WebElement AddPIN;
 	
 	@FindBy(xpath="/html/body/app-root/app-layout/app-faculty-profile/div/div[2]/div/div/div[1]/div[18]/div/td/div/label/input")
 	public static WebElement CheckGroup;
